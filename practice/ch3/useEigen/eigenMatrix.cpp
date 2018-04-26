985ce3abdfeea1c0bbcec9c59059a9104697d711
zhouyi.tan <tzyone@qq.com>
2018-04-26 23:20:09 +0800

    add code for use eigen

--- practice/ch3/useEigen/eigenMatrix.cpp
+++ practice/ch3/useEigen/eigenMatrix.cpp
@@ -53,7 +53,19 @@ int main(int argc, char** argv) {
     std::cout << "Eigen vectors = " << std::endl << eigen_solver.eigenvectors() << std::endl;
 
     // solve equation
-    
+    Eigen::Matrix<double, MATRIX_SIZE, MATRIX_SIZE> matrix_NN;
+    matrix_NN = Eigen::MatrixXd::Random(MATRIX_SIZE, MATRIX_SIZE);
+    Eigen::Matrix<double, MATRIX_SIZE, 1> v_Nd;
+    v_Nd = Eigen::MatrixXd::Random(MATRIX_SIZE, 1);
+
+    clock_t time_start = clock();
+
+    Eigen::Matrix<double, MATRIX_SIZE, 1> x = matrix_NN.inverse() * v_Nd;
+    std::cout << "time use in normal inverse is :" << 1000 * (clock() - time_start)/(double)CLOCKS_PER_SEC << "ms" << std::endl;
+
+    time_start = clock();
+    x = matrix_NN.colPivHouseholderQr().solve(v_Nd);
+    std::cout << "time use in QR composition is :" << 1000 * (clock() - time_start)/(double)CLOCKS_PER_SEC << "ms" << std::endl;
 
     return 0;
 }
